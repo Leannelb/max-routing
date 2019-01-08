69bbf6f113bb157591d11dcb2a012d259857e552
leanne <leanne.lacey@justsomecoding.com>
2019-01-08 14:19:06 +0100

    add path to components in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { Routes } from '@angular/router'
+import { UserComponent } from './users/user/user.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'server', component: ServersService},
+];
 
 @NgModule({
   declarations: [
